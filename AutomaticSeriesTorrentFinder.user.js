24ebfb82cb8e375c1e4680935b3511fa12fb0f4b
Sátán Ádám <satanadam@outlook.com>
2016-11-01 12:04:25 +0100

    Update AutomaticSeriesTorrentFinder.user.js

--- AutomaticSeriesTorrentFinder.user.js
+++ AutomaticSeriesTorrentFinder.user.js
@@ -2,7 +2,7 @@
 // @name         Automatic Series Torrent Finder
 // @namespace    http://www.pogdesign.co.uk/cat/
 // @include      http://www.pogdesign.co.uk/cat/
-// @version      1.0
+// @version      1.1
 // @description  searches for todays episodes in glodls
 // @author       iasatan
 // ==/UserScript==
@@ -10,8 +10,12 @@
 
 (function() {
     var today = document.getElementById("today").parentElement.children;
-    for(i=1;i<today.length;i++)
+    if(confirm("search them?"))
     {
-        open("http://glodls.to/search_results.php?search="+today[i].children[0].children[2].children[0].innerText+"&incldead=Search",i);
+        for(i=1;i<today.length;i++)
+        {
+            if(!today[i].className.includes("checked"))
+                open("http://glodls.to/search_results.php?search="+today[i].children[0].children[2].children[0].innerText+"&incldead=Search",i);
+        }
     }
 })();
