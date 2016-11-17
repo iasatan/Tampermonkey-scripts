343a1b65d13fa3aad9fc147f2cef5868858754cf
Sátán Ádám <satanadam@outlook.com>
2016-11-17 09:36:45 +0100

    button added

--- TorrentHelper.user.js
+++ TorrentHelper.user.js
@@ -2,25 +2,40 @@
 // @name         Torrent Helper
 // @namespace    https://thepiratebay.org/search/
 // @include      https://thepiratebay.org/search/*
-// @version      1.0
+// @version      1.2
 // @description  opens glodls if piratebay doesn't have the neceserry files
 // @author       iasatan
 // ==/UserScript==
+
+function GloDLSSearch()
+{
+    var url = window.location.href.split("/")[4];
+    open("http://glodls.to/search_results.php?search="+url+"&incldead=Search","_");
+}
 
 function openGloDLS()
 {
     var torrents = $('.detName');
     for(i=0;i<torrents.length;i++)
-        if(torrents[i].innerText.includes("720p"))
+        if(torrents[i].innerText.includes("720p"||"1080p"))
             return false;
     return true;
 }
 
 (function() {
-    var url = window.location.href.split("/")[4];
-    if(openGloDLS())
+    var SearchResults = document.getElementById("SearchResults");
+    if(document.referrer.includes("pogdesign"))
     {
-        open("http://glodls.to/search_results.php?search="+url+"&incldead=Search","_");
-        close();
+        if(openGloDLS())
+        {
+            GloDLSSearch();
+            close();
+        }
+        var pirateSerch = document.getElementsByClassName("submitbutton");
+        var btn = document.createElement("input");
+        btn.type = "button";
+        btn.value = "SearchGloDLS";
+        btn.onclick=GloDLSSearch;
+        SearchResults.appendChild(btn);
     }
 })();
