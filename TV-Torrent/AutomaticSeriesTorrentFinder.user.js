d6fdb0549735c78ffffab4c4b29ddfb6521582ec
Sátán Ádám <satanadam@outlook.com>
2016-12-13 08:11:21 +0100

    Update AutomaticSeriesTorrentFinder.user.js

--- TV-Torrent/AutomaticSeriesTorrentFinder.user.js
+++ TV-Torrent/AutomaticSeriesTorrentFinder.user.js
@@ -2,7 +2,7 @@
 // @name         Automatic Series Torrent Finder
 // @namespace    http://www.pogdesign.co.uk/cat/
 // @include      http://www.pogdesign.co.uk/cat/
-// @version      1.2
+// @version      1.3
 // @description  searches for todays episodes in glodls
 // @author       iasatan
 // ==/UserScript==
@@ -17,12 +17,10 @@ function searchThem()
                 if(today[i].children[0].children[2].children[0].innerText==="Agents of SHIELD")
                 {
                     open("https://thepiratebay.org/search/Agents%20of%20"+today[i].children[0].children[2].children[1].innerText,i+"a");
-                    open("http://glodls.to/search_results.php?search=Agents+of&incldead=Search",i+"b");
                 }
                 else
                 {
                     open("https://thepiratebay.org/search/"+today[i].children[0].children[2].children[0].innerText+"%20"+today[i].children[0].children[2].children[1].innerText,i+"a");
-                    open("http://glodls.to/search_results.php?search="+today[i].children[0].children[2].children[0].innerText+"+"+today[i].children[0].children[2].children[1].innerText+"&incldead=Search",i+"b");
                 }
             }
         }
@@ -30,6 +28,8 @@ function searchThem()
 function checkButtonNecesity()
 {
     var today = document.getElementById("today").parentElement.children;
+    if(today.length==1)
+        close();
     for(i=1;i<today.length;i++)
     {
         if(!today[i].className.includes("checked"))
