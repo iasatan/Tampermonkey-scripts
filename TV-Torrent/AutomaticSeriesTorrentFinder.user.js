d615a86456d3210986225971dbccd33a4d3fb753
Sátán Ádám <satanadam@outlook.com>
2017-02-22 15:26:59 +0100

    Update AutomaticSeriesTorrentFinder.user.js

--- TV-Torrent/AutomaticSeriesTorrentFinder.user.js
+++ TV-Torrent/AutomaticSeriesTorrentFinder.user.js
@@ -2,45 +2,45 @@
 // @name         Automatic Series Torrent Finder
 // @namespace    http://www.pogdesign.co.uk/cat/
 // @include      http://www.pogdesign.co.uk/cat/
-// @version      1.3
+// @version      1.5
 // @description  searches for todays episodes in glodls
 // @author       iasatan
 // ==/UserScript==
 
-function searchThem()
-{
+function searchThem(){
     var today = document.getElementById("today").parentElement.children;
-    for(i=1;i<today.length;i++)
-        {
-            if(!today[i].className.includes("checked"))
-            {
-                if(today[i].children[0].children[2].children[0].innerText==="Agents of SHIELD")
-                {
+    for(i=1;i<today.length;i++){
+            if(!today[i].className.includes("checked")){
+                if(today[i].children[0].children[2].children[0].innerText==="Agents of SHIELD"){
                     open("https://thepiratebay.org/search/Agents%20of%20"+today[i].children[0].children[2].children[1].innerText,i+"a");
                 }
-                else
-                {
+                else{
                     open("https://thepiratebay.org/search/"+today[i].children[0].children[2].children[0].innerText+"%20"+today[i].children[0].children[2].children[1].innerText,i+"a");
                 }
             }
         }
 }
-function checkButtonNecesity()
-{
+function checkButtonNecesity(){
     var today = document.getElementById("today").parentElement.children;
     if(today.length==1)
         close();
-    for(i=1;i<today.length;i++)
-    {
+    for(i=1;i<today.length;i++){
         if(!today[i].className.includes("checked"))
            return true;
     }
     return false;
 }
 
+
 (function() {
-    if(checkButtonNecesity())
-    {
+    if(new Date().getDay()==3){
+        var btn2 = document.createElement("input");
+        btn2.type = "button";
+        btn2.value = "Search Music";
+        btn2.onclick=function(){open("http://glodls.to/search_results.php?search=mp3+new&incldead=Search");};
+        today.appendChild(btn2);
+    }
+    if(checkButtonNecesity()){
         var btn = document.createElement("input");
         btn.type = "button";
         btn.value = "Search";
