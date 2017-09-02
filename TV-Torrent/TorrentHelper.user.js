aa697e09106726f3c0866390bf7283f6a673ed81
Sátán Ádám <satanadam@outlook.com>
2017-09-02 06:47:07 +0200

    Blocks other scripsts, glodls search is always there

--- TV-Torrent/TorrentHelper.user.js
+++ TV-Torrent/TorrentHelper.user.js
@@ -2,10 +2,15 @@
 // @name         Torrent Helper
 // @namespace    https://thepiratebay.org/search/
 // @include      https://thepiratebay.org/search/*
-// @version      1.2
+// @version      1.3
 // @description  opens glodls if piratebay doesn't have the neceserry files
 // @author       iasatan
 // ==/UserScript==
+var scripts = document.getElementsByTagName("script");
+for(i=0; scripts.length;i++){
+    scripts[i].parentElement.removeChild(scripts[i]);
+    i--;
+}
 
 function GloDLSSearch()
 {
@@ -23,7 +28,7 @@ function openGloDLS()
 }
 
 (function() {
-    var SearchResults = document.getElementById("SearchResults");
+    var header = document.getElementById("header");
     if(document.referrer.includes("pogdesign"))
     {
         if(openGloDLS())
@@ -31,11 +36,11 @@ function openGloDLS()
             GloDLSSearch();
             close();
         }
-        var pirateSerch = document.getElementsByClassName("submitbutton");
+    }
+    var pirateSerch = document.getElementsByClassName("submitbutton");
         var btn = document.createElement("input");
         btn.type = "button";
         btn.value = "SearchGloDLS";
         btn.onclick=GloDLSSearch;
-        SearchResults.appendChild(btn);
-    }
+        header.appendChild(btn);
 })();
